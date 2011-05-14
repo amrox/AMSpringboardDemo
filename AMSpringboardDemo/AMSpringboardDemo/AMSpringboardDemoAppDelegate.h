da10a54b9316d1a2074708551fbea7ffd1d09f0f
Andy Mroczkowski <andy@mrox.net>
2011-05-13 22:30:38 -0700

    initial commit

---
+++ AMSpringboardDemo/AMSpringboardDemo/AMSpringboardDemoAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AMSpringboardDemoAppDelegate.h
+//  AMSpringboardDemo
+//
+//  Created by Andy Mroczkowski on 4/6/11.
+//  Copyright 2011 MindSnacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMSpringboardDemoAppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
+
+@end
